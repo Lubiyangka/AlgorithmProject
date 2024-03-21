a45c438bf0e8ebac4019dc8f8a32ea8bafd4ce09
John Six <1424157706@qq.com>
2024-03-21 22:18:07 +0800

    搜索-符环

--- OOP/搜索/4-A.cpp
+++ OOP/搜索/4-A.cpp
@@ -2,40 +2,39 @@
 #include <cstring>
 using namespace std;
 
-int t, len, ans;
-bool flag;
-// char s[105];
-string sp;
+const int mn = 55;
+const int inf = 1e9 +7;
 
-bool isMatched(int x, int y){
-    return x < len / 2 && y < len / 2;
-}
+long long f[mn][mn][mn];
+string s;
+int n, t;
+long long ans;
 
 int main(){
     cin >> t;
     while(t--){
-        cin >> sp;
-        len = (int) sp.length();
-        flag = true;
+        cin >> s;
+        n = (int) s.length();
         ans = 0;
-        int x = 1, y = 1;
-        if(sp[0] == 'S') x++;
-        else y++;
-        if(sp[len - 1] == 'S') x++;
-        else y++;
-        if(!isMatched(x, y)) {
-            cout << ans << endl;
-            continue;
-        }
-        for(int i = 1; i < len - 1 && flag; i++){
-            if(!isMatched(x, y)) {
-                flag = false;
-                continue;
+        for(int l = 0; l <= n; l++){
+            memset(f, 0, sizeof(f));
+            f[0][0][l] = 1;
+            for(int i = 1; i <= n; i++){
+                for(int j = 0; j <= n; j++){
+                    for(int k = 0; k <= n; k++){
+                        if (s[i - 1] == 'S') {
+                            if (j - 1 >= 0 && k - 1 >= 0) f[i][j][k] += f[i - 1][j - 1][k - 1];
+                            if (j + 1 <= n && k + 1 <= n) f[i][j][k] += f[i - 1][j + 1][k + 1];
+                        } else {
+                            if (j - 1 >= 0 && k + 1 <= n) f[i][j][k] += f[i - 1][j - 1][k + 1];
+                            if (j + 1 <= n && k - 1 >= 0) f[i][j][k] += f[i - 1][j + 1][k - 1];
+                        }
+                    }
+                }
             }
-            if(sp[i] == 'S'){
-                if(x < len / 2) x += 2;
-                else y += 2; 
-            }else x++, y++;
+            ans += f[n][l][0];
         }
+        cout << ans << endl;
     }
+    return 0;
 }
